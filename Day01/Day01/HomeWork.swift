ae860356fd560e17f00e16debb1ba3fe3c25dabc
Trung Nghiem <nghiem.duc.trung@gmail.com>
2019-10-09 22:46:51 +0700

    Bai 2

--- Day01/Day01/HomeWork.swift
+++ Day01/Day01/HomeWork.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-//  1, Cho bán kính hình cầu, tính và in ra diện tích, thể tích của hình cầu đó.
+// ----------------Bai 1----------------
+//  Cho bán kính hình cầu, tính và in ra diện tích, thể tích của hình cầu đó.
 //  Hướng dẫn: S = 4πR 2 và V = (4/3)πR 3
 func Bai1 () {
     print("Nhap ban kinh hinh cau: ")
@@ -25,7 +26,18 @@ func TheTichHinhCau(radius : Double) -> Double {
     return (4/3)*3.1415*radius
 }
 
-//  Variable 'radius' was never mutated; consider changing to 'let' constant
-func <#name#>(<#parameters#>) -> <#return type#> {
-    <#function body#>
+//--------------Bai 2--------------
+//Tính tổng bình phương 2 số a, b (a, b nhập từ bàn phím)
+func TongBinhPhuong2So(a: Double, b: Double) -> Double {
+    return (a*a)+(b*b)
 }
+
+func Bai2() {
+    print("Nhap so thu nhat: ")
+    var a = Double(readLine()!)!
+    print("nhap so thu hai:")
+    var b = Double(readLine()!)!
+    print("Tong binh phuon 2 so vuwa nhap la \(TongBinhPhuong2So(a: a, b: b))")
+}
+
+//----------------Bai 3----------------
