96663451c9b825c85bf76778ea0ce6cb0fa1b823
Trung Nghiem <nghiem.duc.trung@gmail.com>
2019-10-26 18:50:30 +0700

    projectUI

---
+++ BaiTapVeNhaUI/BaiTapVeNhaUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTapVeNhaUI
+//
+//  Created by Trung on 10/26/19.
+//  Copyright © 2019 Trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
