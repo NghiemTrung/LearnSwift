3f57a849363e7f245a3b99c6f9dea38da3957bfc
Trung Nghiem <nghiem.duc.trung@gmail.com>
2020-01-13 13:16:41 +0700

    Day25

---
+++ Day25-BTVN/Day25-BTVN/Model/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  Day25-BTVN
+//
+//  Created by Trung on 1/13/20.
+//  Copyright © 2020 Trung. All rights reserved.
+//
+
+import Foundation
---
+++ Day25-BTVN/Day25-BTVN/View/listItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  listItemTableViewCell.swift
+//  Day25-BTVN
+//
+//  Created by Trung on 1/12/20.
+//  Copyright © 2020 Trung. All rights reserved.
+//
+
+import UIKit
+
+class listItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
